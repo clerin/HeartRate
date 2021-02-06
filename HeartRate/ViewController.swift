37f63aab6851e68ebcbc3e1e921e59c00b57d99c
clerin <clerinbinil@gmail.com>
2021-02-06 11:50:07 -0500

    new changes

--- HeartRate/ViewController.swift
+++ HeartRate/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("My heart raste is 75 bpm")
     }
 
 
